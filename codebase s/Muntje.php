07799e1d9a912a001265866dfa85972112444708
GertNingbers <97081115@st.deltion.nl>
2024-09-24 11:03:07 +0200

    V2 muntje

--- codebase s/Muntje.php
+++ codebase s/Muntje.php
@@ -1,11 +1,18 @@
 <?php
 
 $random = rand(1,2);
+$stop = 0;
 
-if ($random == 1) {
-    echo "Het is Kop!\n";
+while ($stop === "Nee") {
+
+  $stop = readline ("Wil je kop of munt spelen [Ja/Nee]: ");
+
+  if ($random === 1) {
+      echo "Het is Kop!\n";
 }
-if ($random == 2) {
-    echo "Het is Munt!\n";
+
+  if ($random === 2) {
+      echo "Het is Munt!\n";
+}
 }
 ?>
