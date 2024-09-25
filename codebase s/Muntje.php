d69bbc353052b863c8467b8e2fcb9b1469432007
GertNingbers <97081115@st.deltion.nl>
2024-09-25 09:08:55 +0200

    V5 Muntje

--- codebase s/Muntje.php
+++ codebase s/Muntje.php
@@ -13,9 +13,9 @@ while ($stop != "nee") {
     exit("Uw wou geen kop of munt spelen.");
   } else { //hier begint de kop of munt pas als alle invoer goed is
     $random = rand(1,2);
-    if ($random === 1) {
+    if ($random == 1) {
       echo "Het is Kop!\n"; 
-    } elseif ($random === 2) {
+    } elseif ($random == 2) {
       echo "Het is Munt!\n";
     }
   }
