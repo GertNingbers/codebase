b7de2f1d37333bfb6d0200381ad1cdd8e418b1b1
GertNingbers <97081115@st.deltion.nl>
2024-09-25 08:36:15 +0200

    V4

--- codebase s/Muntje.php
+++ codebase s/Muntje.php
@@ -2,19 +2,19 @@
 
 $stop = "";
 
-while ($stop != "Nee") {
+while ($stop != "nee") {
   $stop = readline("Wil je kop of munt spelen [Ja/Nee]: ");
   $stop = strtolower($stop); // zet de invoer om naar kleine letters
 
-  if ($stop != "Ja" && $stop != "Nee") {
+  if ($stop != "ja" && $stop != "nee") {
     echo "Fout je mag alleen 'Ja' of 'Nee' typen.\n";
     $stop = ""; // $stop wordt hier gereset zodat hij weer kan loopen
-  } elseif ($stop == "Nee") {
+  } elseif ($stop == "nee") {
     exit("Uw wou geen kop of munt spelen.");
-  } else {
+  } else { //hier begint de kop of munt pas als alle invoer goed is
     $random = rand(1,2);
     if ($random === 1) {
-      echo "Het is Kop!\n";
+      echo "Het is Kop!\n"; 
     } elseif ($random === 2) {
       echo "Het is Munt!\n";
     }
