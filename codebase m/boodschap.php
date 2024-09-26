9ab6fced50766b831020172d03d9da624d9042e3
GertNingbers <97081115@st.deltion.nl>
2024-09-26 15:55:16 +0200

    V2 boodschap

--- codebase m/boodschap.php
+++ codebase m/boodschap.php
@@ -1,28 +1,23 @@
 <?Php
 
-
-while(true){
-    $hoeveel = readline ("Hoeveel boodschappen wil je doen? \n");
-    if (is_numeric($hoeveel)){
-     break;
-    } 
-     else {
-       echo "!!!Je moet een cijfer invoeren!!!\n";
+while (true) {
+    $hoeveel = readline("Hoeveel boodschappen wil je doen? \n");
+    if (is_numeric($hoeveel)) {
+        break;
+    } else {
+        echo "!!!Je moet een cijfer invoeren!!!\n";
     }
 }
 
 $begin = 1;
-$lijst = array ();
+$lijst = array();
 
-for ($teller=$begin; $teller<=$hoeveel; $teller++) {
-   $boodschap = readline ("welke boodschap wil je toevoegen.\n");
-   array_push($lijst, $boodschap); 
-   var_dump($boodschap);
+for ($teller = $begin; $teller <= $hoeveel; $teller++) {
+    $boodschap = readline("welke boodschap wil je toevoegen.\n");
+    array_push($lijst, $boodschap);
+    var_dump($boodschap);
 }
 
 echo var_export($lijst);
 echo "\n"
-
-
-
 ?>
