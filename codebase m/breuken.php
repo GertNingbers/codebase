f105ff29dff6b92c54a791d89af25246c4f09c70
GertNingbers <97081115@st.deltion.nl>
2024-09-30 09:55:09 +0200

    V1 breuken

---
+++ codebase m/breuken.php
@@ -0,0 +1,19 @@
+<?php
+
+while(true){
+    $nummer1 = readline("Geef het DEELTAL hier: ");
+    $nummer2 = readline ("En hier de DELER: ");
+    if (is_numeric($nummer1) &&  is_numeric($nummer2)){
+      break;
+    }
+    else {
+        echo "Je moet nummers invoeren.\n";
+    }
+}
+
+$som = $nummer1 /  $nummer2;
+$som = round($som, 2);
+
+echo "Deeltal : $nummer1\nDeler : $nummer2\nAntwoord: $som" ;
+
+?>
