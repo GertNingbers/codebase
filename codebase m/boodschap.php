f820a5e764c143f4d7af917e8a349fef61473d7d
GertNingbers <97081115@st.deltion.nl>
2024-09-30 09:08:56 +0200

    V3 boodschap

--- codebase m/boodschap.php
+++ codebase m/boodschap.php
@@ -1,5 +1,6 @@
 <?Php
 
+//dit zorgt ervoor dat er een nummer invoer is.
 while (true) {
     $hoeveel = readline("Hoeveel boodschappen wil je doen? \n");
     if (is_numeric($hoeveel)) {
@@ -12,12 +13,13 @@ while (true) {
 $begin = 1;
 $lijst = array();
 
+//hier wordt de invoer opgeslagen in een array en ook een lijstje gemaakt.
 for ($teller = $begin; $teller <= $hoeveel; $teller++) {
     $boodschap = readline("welke boodschap wil je toevoegen.\n");
     array_push($lijst, $boodschap);
-    var_dump($boodschap);
+        $string=implode("\n",$lijst);
 }
 
-echo var_export($lijst);
-echo "\n"
+echo $string;
+
 ?>
