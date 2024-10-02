f5898079026e88fe1f057d44169ca469b496ae2b
GertNingbers <97081115@st.deltion.nl>
2024-10-02 09:22:15 +0200

    V4 galgje

--- codebase m/galg1.php
+++ codebase m/galg1.php
@@ -1,6 +1,8 @@
 <?php
 
-$lijst = array("appel", "eikenboom", "koffie", "kasteel", "boeken", "kameel", "dolfijn", "orca", "hond", "stad", "voetbal", "geluk", "nederland", "holland", "schip", "zacht", "vocht", "wazig", "glijd", "afzak", "aarde", "cijfer", "engel", "aarde");
+$lijst = array("appel", "eikenboom", "koffie", "kasteel", "boeken", "kameel", "dolfijn", "orca", "hond", "stad", "voetbal", "geluk", "nederland", "holland", "schip", "zacht", "vocht", "wazig", "glijd", "afzak", "aarde", "cijfer", "engel", "aarde", "topper");
+$aantal = count($lijst);
+$som = $aantal - 1;
 
 while (true) {
     $vraag = readline("Wil je galgje spelen [Ja/Nee]?: ");
@@ -11,8 +13,8 @@ while (true) {
     } elseif ($vraag == "nee") {
         exit("Je wou geen galgje spelen.");
     } elseif ($vraag == "ja") {
-        $random = rand(0,23);
-        $woord = $lijst [$random];
+        $random = rand(0, $som);
+        $woord = $lijst[$random];
         $woordLengte = strlen($woord);
         $geradenLetters = array_fill(0, $woordLengte, "_");
         $fouten = 0;
@@ -24,9 +26,7 @@ while (true) {
             $letter = readline("Geef een letter: ");
             $letter = strtolower($letter);
             $beurten++;
-            if (is_numeric($letter)){
-                echo "Je mag alleen maar letters invoeren!!!\n";
-            }
+
             if (strlen($letter) != 1) {
                 echo "Je mag alleen één letter invoeren!!!\n";
             } elseif (strpos($woord, $letter) !== false) {
@@ -40,6 +40,9 @@ while (true) {
                     echo "je hebt er $beurten beurten over gedaan. \n";
                     break;
                 }
+            }
+            if (is_numeric($letter)) {
+                echo "Je mag alleen maar letters invoeren!!!\n";            
             } else {
                 $fouten++;
                 echo "Fout! Je hebt nog $maxFouten - $fouten pogingen over.\n";
@@ -48,7 +51,7 @@ while (true) {
                     echo "je hebt er $beurten beurten over gedaan. \n";
                     break;
                 }
-            } 
+            }
         }
     } else {
         echo "Je mag alleen ja of nee typen!!!.\n";
